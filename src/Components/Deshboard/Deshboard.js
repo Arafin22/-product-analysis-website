767d07cd08520768fd361702c762c212601070eb
arafin0 <sultanularafin000@gmail.com>
2022-04-04 16:14:38 +0600

    rechart added

--- src/Components/Deshboard/Deshboard.js
+++ src/Components/Deshboard/Deshboard.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Deshboard.css'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line, ComposedChart} from 'recharts';
+
 
 const Deshboard = () => {
-    return (
-        <div>
-            <h1>deshboard</h1>
+    const [chartdata, setChartdata]=useState([]);
+    console.log(chartdata);
+
+
+    useEffect(() =>{
+        fetch("data1.json")
+        .then((res) => res.json())
+        .then((data) => setChartdata(data));
+    },[])
+
+      return (
+          <div className='top-class'>
+
+        <div width="100%" height="100%">
+        <h2>SimpleBarChart</h2>
+           <BarChart
+          width={500}
+          height={300}
+          data={chartdata}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="sell" fill="#8884d8" />
+          <Bar dataKey="revenue" fill="#82ca9d" />
+        </BarChart>
         </div>
-    );
+
+
+        <div width="100%" height="100%">
+        <ComposedChart
+          width={500}
+          height={400}
+          data={chartdata}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="month" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="revenue" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="sell" stroke="#ff7300" />
+        </ComposedChart>
+      </div>
+
+
+    
+        </div>
+      );
+      
+
 };
 
 export default Deshboard;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
+
+import image from '../../Assets/perfume.jpeg'
+
+import './Home.css'
 
 const Home = () => {
+
+    const [reviews] =useReviews();
     return (
         <div>
-            <h1>home</h1>
+              <div>
+      <div className="container">
+          <div className="row">
+              
+        
+              <div className="col-sm-12 col-md-6 col-lg-6 col-12 ">
+                  <div className="align-items-center">
+                  <div className="pt-5">
+                    <h2>Choose Your Perfume</h2>
+                    <p>Perfumes hut is an Online Perfume Shop in Bangladesh. That Sells Original Branded Perfumes and Attar (Alcohol Free Perfumes) at The Best Price in Bangladesh.</p>
+                    <button type="button" className="bg-light text-primary btn  btn-lg ">Buy Now</button>
+  
+  
+                   
+                  </div>
+                  </div>
+              </div>
+
+              <div className="col-sm-12 col-md-6 col-lg-6 col-12">
+                  <img className="image-size img-fluid" src={image} alt=""/>
+              </div>
+          </div>
+          
+      </div>
+  </div>
+
+
+           
+            <div className='reviews-parrent'>
+            
+            {
+                reviews.slice(1,4).map((review) => <Review 
+                key={review._id}
+                review={review}
+                />)
+            }
+        </div>
+            <button className="bg-light text-primary btn  btn-lg ">
+                <Link to='/reviews'> Reviews</Link>
+            </button>
         </div>
     );
 };
